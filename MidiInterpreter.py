95c525af1e5a91b68d0113cbfda3af7ac1b21260
Tyson Smith <tyson.w.smith@gmail.com>
2016-05-14 23:44:40 -0700

    Clean up and make Pylint happier

--- MidiInterpreter.py
+++ MidiInterpreter.py
@@ -1,5 +1,5 @@
-from Interpreter import *
-from MIDILike import *
+from Interpreter import SongInterpreter
+from MIDILike import MIDILike, MIDILikeTrack
 from MIDIEvent import *
 
 class MIDIInterpreter(SongInterpreter):
@@ -36,32 +36,37 @@ class MIDIInterpreter(SongInterpreter):
             0x04: TrackNameEvent,
             0x05: InstrumentNameEvent
         }
+        self.lastgoodbyte = None
 
-    def from_twos_comp(self, n, bits=8):
+    @staticmethod
+    def from_twos_comp(n, bits=8):
         """Convert two's compliment representation of n"""
         f = 0
-        for i in range(bits):
+        for _ in range(bits):
             f <<= 2
             f += 1
         return (n - 1).__xor__(f)
 
-    def to_twos_comp(self, n, bits=8):
+    @staticmethod
+    def to_twos_comp(n, bits=8):
         """Get two's compliment representation of n"""
         f = 0
-        for i in range(bits):
+        for _ in range(bits):
             f <<= 2
             f += 1
         return n.__xor__(f)
 
-    def pop_n(self, queue, nbytes=1):
+    @staticmethod
+    def pop_n(queue, nbytes=1):
         """Get first N bytes from byte list"""
         out = 0
-        for i in range(nbytes):
+        for _ in range(nbytes):
             out <<= 8
             out += int(queue.pop(0))
         return out
 
-    def get_variable_length(self, queue):
+    @staticmethod
+    def get_variable_length(queue):
         """Calculate variable length integer from byte list"""
         n = 0
         while True:
@@ -74,20 +79,23 @@ class MIDIInterpreter(SongInterpreter):
 
     def process_mtrk_event(self, firstbyte, queue, current_deltatime, track):
         channel = firstbyte & 0x0F
+
         # Channel Voice Message
         if int(firstbyte >> 4) in (8, 9, 10, 11, 14):
             b = self.pop_n(queue)
             c = self.pop_n(queue)
             if int(firstbyte >> 4) == 9 and c == 0:
                 firstbyte &= 0xEF
-            midi_event = self.cve[int(firstbyte >> 4)](channel, b, c)
-            track.add_event(current_deltatime, midi_event)
-            return firstbyte
+            track.add_event(
+                current_deltatime,
+                self.cve[int(firstbyte >> 4)](channel, b, c))
+
         elif int(firstbyte >> 4) in (12, 13):
             b = self.pop_n(queue)
-            midi_event = self.cve[int(firstbyte >> 4)](channel, b)
-            track.add_event(current_deltatime, midi_event)
-            return firstbyte
+            track.add_event(
+                current_deltatime,
+                self.cve[int(firstbyte >> 4)](channel, b))
+
         # System Common Message
         elif firstbyte == 0xF0:
             dump = []
@@ -95,27 +103,30 @@ class MIDIInterpreter(SongInterpreter):
                 x = self.pop_n(queue)
                 if x == 0xF7:
                     break
-                else:
-                    dump.append(x)
-            midi_event = SystemExclusiveEvent(firstbyte, dump)
-            track.add_event(current_deltatime, midi_event)
+                dump.append(x)
+            track.add_event(current_deltatime, SystemExclusiveEvent(firstbyte, dump))
+
         elif firstbyte == 0xF2: # Song Position pointer
             nb = self.pop_n(queue)
             nc = self.pop_n(queue)
-            midi_event = SongPositionPointerEvent((nc & 0x7) + ((nb & 0x7) << 7))
-            track.add_event(current_deltatime, midi_event)
+            track.add_event(
+                current_deltatime,
+                SongPositionPointerEvent((nc & 0x7) + ((nb & 0x7) << 7)))
 
         elif firstbyte == 0xF3:
             b = self.pop_n(queue)
             track.add_event(current_deltatime, SongSelectEvent(b))
+
         elif firstbyte == 0xF6:
             track.add_event(current_deltatime, self.se[firstbyte]())
+
         elif firstbyte == 0xF7:
             vlen = self.get_variable_length(queue)
             data = self.pop_n(queue, vlen)
             track.add_event(current_deltatime, SystemExclusiveEvent(0xF0, data))
+
         elif firstbyte in (0xF1, 0xF4, 0xF5): # Undefined
-            return 0
+            return 0 # TODO: isn't this handled by the else case?
 
         # Meta-Event
         elif firstbyte == 0xFF:
@@ -125,17 +136,19 @@ class MIDIInterpreter(SongInterpreter):
             self.pop_n(queue, v_length)
             # Deal with Meta Events Later
             if meta_type in self.me.keys():
-                midi_event = self.me[meta_type](event_data_bytes)
-                track.add_event(current_deltatime, midi_event)
+                track.add_event(current_deltatime, self.me[meta_type](event_data_bytes))
+
         # System Real-Time Message
         elif firstbyte >= 0xF8:
-            midi_event = SystemRealTimeEvent()
-            track.add_event(current_deltatime, midi_event)
+            track.add_event(current_deltatime, SystemRealTimeEvent())
+
         elif firstbyte < 128:
-           queue.insert(0, firstbyte)  
-           return self.process_mtrk_event(self.lastgoodbyte, queue, current_deltatime, track)
+            queue.insert(0, firstbyte)
+            return self.process_mtrk_event(self.lastgoodbyte, queue, current_deltatime, track)
+
         else:
             return 0
+
         return firstbyte
 
 
@@ -145,7 +158,6 @@ class MIDIInterpreter(SongInterpreter):
         tpqn = 120
         with open(midifile, 'rb') as fp:
             queue = bytearray(fp.read())
-            ml = fp.tell()
         self.lastgoodbyte = 0x90
         mlo = MIDILike()
         chunkcount = {}
@@ -159,30 +171,29 @@ class MIDIInterpreter(SongInterpreter):
             track = MIDILikeTrack()
             current_deltatime = 0
             if chunk_type == 'MThd':
-                size = self.pop_n(queue, 4)
+                self.pop_n(queue, 4) # pop size
                 midi_format = self.pop_n(queue, 2)
-                num_tracks = self.pop_n(queue, 2)
+                self.pop_n(queue, 2) # pop num_tracks
                 divword = self.pop_n(queue, 2)
                 if divword & 0x8000:
                     neg = int((divword & 0x7F00) >> 8)
                     SMPTE = self.from_twos_comp(neg, 7)
                     tpf = divword & 0x00FF
-                    print ("SMPTE", SMPTE, "TPF:", tpf)
+                    print("SMPTE", SMPTE, "TPF:", tpf)
                 else:
                     tpqn = divword & 0x7FFF
-                    print ("ticks per quarter note:", tpqn)
+                    print("ticks per quarter note:", tpqn)
                 mlo.set_tpqn(tpqn)
                 mlo.set_format(midi_format)
             elif chunk_type == 'MTrk':
                 length = self.pop_n(queue, 4)
-                subqueue = queue[0:length]
+                subqueue = queue[:length]
                 queue = queue[length:]
                 while subqueue:
-                    vlen = self.get_variable_length(subqueue)
-                    current_deltatime += vlen
+                    current_deltatime += self.get_variable_length(subqueue)
                     n = self.pop_n(subqueue)
                     gb = self.process_mtrk_event(n, subqueue, current_deltatime, track)
-                    if gb & 0xf0 == 0x90: 
+                    if gb & 0xf0 == 0x90:
                         self.lastgoodbyte = gb
             else:
                 break
