a90ae33247a2b0225584f1e80dda4a10815d6efe
Quintin Smith <smith.f.quintin@gmail.com>
2016-08-06 19:09:19 -0700

    Incorporated a windowing system

--- Interface.py
+++ Interface.py
@@ -5,80 +5,36 @@ from MIDILike import *
 from Player import Player
 from MidiInterpreter import MIDIInterpreter
 from MIDIController import *
-from getCh import getCh
+from Box import BoxEnvironment, Box
+from Interactor import Interactor
 
-class InputNode(object):
-    def __init__(self):
-        self.children = {}
-        self.action = None
-        self.args = None
-
-    def set(self, path, action, args=None):
-        if path:
-            n = path[0]
-            path = path[1:]
-            if not n in self.children.keys():
-                self.children[n] = InputNode()
-            self.children[n].set(path, action, args)
-        else:
-            self.action = action
-            self.args = args
-
-    def get(self, path):
-        if path:
-            n = path[0]
-            path = path[1:]
-            if n in self.children.keys():
-                return self.children[n].get(path)
-            else:
-                return None
-        else:
-            return self
-
-
-class Interface(object):
+class Interface(BoxEnvironment, Interactor):
     def __init__(self, cache_path='.cache.json'):
-        if os.path.isfile(cache_path):
-            with open(cache_path, 'r') as fp:
-                cache = json.loads(fp.read())
-        else:
-            cache = {}
-        self.active_threads = []
-        self.player = Player()
-        self.number_register = 0
-        self.cmdNode = InputNode()
-        self.cmdNode.set('q', self.quit)
-        self.cmdNode.set('l', self.player.next_state)
-        self.cmdNode.set('o', self.player.prev_state)
-        self.cmdNode.set('0', self.set_register, 0)
-        self.cmdNode.set('1', self.set_register, 1)
-        self.cmdNode.set('2', self.set_register, 2)
-        self.cmdNode.set('3', self.set_register, 3)
-        self.cmdNode.set('4', self.set_register, 4)
-        self.cmdNode.set('5', self.set_register, 5)
-        self.cmdNode.set('6', self.set_register, 6)
-        self.cmdNode.set('7', self.set_register, 7)
-        self.cmdNode.set('8', self.set_register, 8)
-        self.cmdNode.set('9', self.set_register, 9)
-        self.cmdNode.set(chr(27), self.clear_register)
-        self.cmdNode.set('t', self.toggle_track)
-        self.listening = False
+        BoxEnvironment.__init__(self)
+        Interactor.__init__(self)
+        self.init_screen()
         self.midi_interpreter = MIDIInterpreter()
-        if 'songs' in cache:
-            self.songsettings = cache['songs']
-        else:
-            self.songsettings = {}
+        self.active_threads = []
+
+        self.listening = False
+
+        self.assign_sequence("q", self.quit)
+
+
+    def show_player(self):
+        player = Player()
+        player.parent = self
+        new_id = self.id_gen
+        self.id_gen += 1
+        self.boxes[new_id] = player
+        w = console.getTerminalSize()[0]
+        self.box_positions[new_id] = ((w - 127) // 2,0)
+        self.player = player
+
 
     def toggle_track(self):
-        self.player.toggle_trackn(self.number_register)
+        self.player.toggle_trackn(self.general_register)
         self.clear_register()
-
-    def set_register(self, n):
-        self.number_register *= 10
-        self.number_register += n
-
-    def clear_register(self):
-        self.number_register = 0
 
     def load_midi(self, midi_path):
         midilike = self.midi_interpreter(midi_path)
@@ -87,6 +43,7 @@ class Interface(object):
     def quit(self):
         self.listening = False
         self.player.quit()
+        self.destroy()
 
     def play_along(self, midilike, hidden=[]):
         thread = threading.Thread(target=self.player.play_along, args=[midilike, MIDIController(), hidden])
@@ -95,38 +52,19 @@ class Interface(object):
 
     def input_loop(self):
         self.listening = True
-        current_path = []
         while self.listening:
-            current_path.append(getCh())
-            node = self.cmdNode.get(current_path)
-            if node:
-                if node.action:
-                    if not node.args is None:
-                        node.action(node.args)
-                    else:
-                        node.action()
-
-                    current_path = []
+            if self.player and self.player.playing: #Kludge
+                self.player.get_input()
             else:
-                current_path = []
+                self.get_input()
+
 import sys
 import console
 if __name__ == '__main__':
+    sys.stdout.write("\033?25l")
     interface = Interface()
     midilike = interface.load_midi(sys.argv[1])
     note_range = midilike.get_note_range()
-
-    w,h = console.getTerminalSize()
-    sys.stdout.write('\033[0;0H')
-    sys.stdout.write('\033[46m%s' % ((w * h) * ' '))
-    sys.stdout.write('\033[0;0H\033[0m')
-    for i in range(7):
-        sys.stdout.write('\033[3%dm%s\033[0m' % (i + 1, hex(i)[2:]))
-    ignore = []
-    for i in sys.argv[2:]:
-        ignore.append(int(i))
-    interface.play_along(midilike, ignore)
+    interface.show_player()
+    interface.play_along(midilike)
     interface.input_loop()
-    sys.stdout.write('\033[0;0H')
-    sys.stdout.write('\033[0m%s' % ((w * h) * ' '))
-
--- Player.py
+++ Player.py
@@ -1,79 +1,78 @@
 import console
 import time
 import sys
-from getCh import getCh
 from MidiInterpreter import *
 from MIDIController import *
+from Box import Box
+from Interactor import Interactor
 
-class Player(object):
+class Player(Box, Interactor):
     """Plays MIDILike Objects"""
     NEXT_STATE = 1
     PREV_STATE = 2
     RAISE_QUIT = 4
-    RAISE_CHANGE = 8
+    RAISE_MIDI_INPUT_CHANGE = 8
+    RAISE_JUMP = 16
 
     SHARPS = (1,3,6,8,10)
 
     sidebar = '|'
 
+    def quit(self):
+        self.set_flag(self.RAISE_QUIT)
+
+    def next_state(self):
+        self.set_flag(self.NEXT_STATE)
+
+    def prev_state(self):
+        self.set_flag(self.PREV_STATE)
+
+    def jump(self):
+        self.set_flag(self.RAISE_JUMP)
+        self.song_position = self.general_register
+        self.clear_register()
+
     def __init__(self):
+        Box.__init__(self)
+        Interactor.__init__(self)
+
+        self.assign_sequence("l", self.next_state)
+        self.assign_sequence("o", self.prev_state)
+        self.assign_sequence("j", self.jump)
+        self.assign_sequence("q", self.quit)
+
         self.note_range = [0, 127]
+        self.playing = False
         self.flags = {}
         self.previously_expected_set = set()
         self.post_buffer = 4
         self.screen_size = console.getTerminalSize()
         self.screen_size = (self.screen_size[0], self.screen_size[1] - 3)
         self.ignore = []
-
-    def toggle_trackn(self, n):
-        if n in self.ignore:
-            self.ignore.remove(n)
-        else:
-            self.ignore.append(n)
-        self.set_flag(self.RAISE_CHANGE)
+        self.song_position = 0
 
     def _get_note_str(self, note, channel=10):
         """Convert Midi Note byte to Legible Character"""
         note_list = 'CCDDEFFGGAAB'
         note %= 12
         if note in self.SHARPS:
-            return '\033[7;3' + str(channel) + 'm' + note_list[note] + '\033[0m'
+            return "\033[7;3%dm%s\033[0m" % (channel + 1, note_list[note])
         else:
-            return '\033[3' + str(channel) + 'm' + note_list[note] + '\033[0m'
-
-    def draw_input_line(self, user_input, expected):
-        num_of_keys = self.note_range[1] - self.note_range[0] + 1
-        x_offset = int((self.screen_size[0] - num_of_keys - 2) / 2) - 1
-        y_offset = self.screen_size[1] - 1 - self.post_buffer
-
-        expected_set = set(expected.keys())
-
-        line = [self._get_note_str(self.note_range[0] - 1)]
-        for i in range(num_of_keys):
-            note = i + self.note_range[0]
-            if note in expected_set:
-                char = self._get_note_str(note, expected[note].channel + 1)
-            else:
-                char = '-'
-            if note in user_input:
-                char = '\033[42m' + char + '\033[0m'
-            line.append(char)
-        line.append(self._get_note_str(self.note_range[1] + 1))
-        sys.stdout.write('\033[' + str(y_offset) + ';' + str(x_offset) + 'H')
-        sys.stdout.write('\033[0m' + ''.join(line) + '\n')
-
+            return "\033[3%dm%s\033[0m" % (channel + 1, note_list[note])
 
     def play_along(self, midilike, controller, hidden=[]):
         """Display notes in console. Main function"""
         if controller.connected:
             controller.listen()
-        self.note_range = midilike.get_note_range()
+        #self.note_range = midilike.get_note_range()
         num_of_keys = self.note_range[1] - self.note_range[0] + 1
+        self.resize(num_of_keys, self.screen_size[1])
+        self.refresh()
         squash_factor = 8 / midilike.tpqn
 
         state_list = []
-        pressed_keys = {}
         for tick in range(len(midilike)):
+            pressed_keys = {}
             for track in midilike.tracks:
                 for event in track.get_events(tick):
                     if event.eid == event.NOTE_ON and event.channel != 10 and not event.channel in hidden:
@@ -83,66 +82,73 @@ class Player(object):
                             pressed_keys[event.note] = event
                     elif event.eid == event.NOTE_OFF and event.note in pressed_keys.keys() and event.channel != 10:
                         del pressed_keys[event.note]
-            state_list.append(pressed_keys.copy())
+
+            if len(pressed_keys.keys()):
+                while len(state_list) <= tick * squash_factor:
+                    state_list.append({})
+                state_list[int(tick * squash_factor)] = pressed_keys.copy()
 
         # Shrink State_list length
         screen_width, screen_height = self.screen_size
-        compressed_state_list = []
-        for y in range(int(len(state_list) * squash_factor)):
-            compressed_state_list.append(state_list[int(y / squash_factor)])
-        state_list = compressed_state_list
+        
         for _ in range(self.post_buffer * 5):
             state_list.insert(0, {})
         for _ in range(screen_height):
             state_list.append({})
 
+        input_box_id = self.add_box(x=0, y=screen_height - 1, width=127, height=1)
+        for x in range(127):
+            self.boxes[input_box_id].set(x, 0, "-")
 
-        song_position = 0
-        x_offset = int((console.getTerminalSize()[0] - num_of_keys - 2) / 2)
-        states_matched = []
-        displayed = []
-        to_clear = [[]] * (screen_height + self.post_buffer)
+        for j in range(len(state_list)):
+            new_bid = self.add_box(x=0, y=screen_height - 1 - j, width=127, height=1)
+            newBox = self.boxes[new_bid]
+            for key, event in state_list[j].items():
+                if event.channel != 10:
+                    newBox.set(key, 0, self._get_note_str(key, event.channel))
 
-        for y in range(screen_height):
-            sys.stdout.write('\033[%d;%dH%s\033[0m\n' % (y, x_offset, (' ' * num_of_keys)))
-
-        while state_list:
-            current_display = state_list[0:screen_height - self.post_buffer][::-1]
-            current_display += states_matched[0:min(self.post_buffer, len(states_matched))]
-            mark_pos = screen_height - int(screen_height * song_position / len(state_list))
-            
-            for y in range(len(current_display)):
-                for x in to_clear[y]:
-                    sys.stdout.write("\033[%d;%dH " % (y,x))
-                line = current_display[y]
-                to_clear[y] = []
-                for key, event in line.items():
-                    sys.stdout.write('\033[%d;%dH' % (y, x_offset + key - self.note_range[0]))
-                    sys.stdout.write(self._get_note_str(key, event.channel + 1))
-                    to_clear[y].append(x_offset + key - self.note_range[0])
-
-            sys.stdout.write('\033[%d;%dH' % (screen_height, x_offset))
-            sys.stdout.write('\n')
-
-            current_state = state_list.pop(0)
-            states_matched.insert(0, current_state)
-            result = self._wait_for_input(current_state, controller)
-            if result == self.NEXT_STATE:
-                song_position += 1
+        self.song_position = 0
+        self.playing = True
+        self.refresh()
+        while self.playing:
+            result = self._wait_for_input(state_list[self.song_position], controller)
+            if result == self.RAISE_QUIT:
+                self.playing = False
+            elif result == self.NEXT_STATE:
+                self.song_position = (self.song_position + 1) % len(state_list)
+                for b_id, pos in self.box_positions.items():
+                    if b_id == input_box_id:
+                        continue
+                    x, y = pos
+                    y = (y + 1) % len(state_list)
+                    self.box_positions[b_id] = (x,y)
+                    self.boxes[b_id].set_refresh_flag()
+                self.refresh()
             elif result == self.PREV_STATE:
-                # return current state to the queue
-                # then return the state before that
-                song_position -= 1
-                state_list.insert(0, states_matched.pop(0))
-                while len(states_matched) > 1 and not len(states_matched[0].keys()):
-                    state_list.insert(0, states_matched.pop(0))
+                first = True
+                while (first or not state_list[self.song_position]) and self.song_position > 0:
+                    first = False
+                    self.song_position = max(0, self.song_position - 1)
+                    for b_id, pos in self.box_positions.items():
+                        if b_id == input_box_id:
+                            continue
+                        x, y = pos
+                        y = (y - 1) % len(state_list)
+                        self.box_positions[b_id] = (x,y)
+                self.refresh()
+            elif result == self.RAISE_JUMP:
+                sorted_keys = list(self.boxes.keys())
+                sorted_keys.sort()
 
-                if states_matched:
-                    state_list.insert(0, states_matched.pop(0))
-
-            elif result == self.RAISE_QUIT:
-                break
-        sys.stdout.write('\n' + (' ' * screen_width))
+                for i in range(len(sorted_keys)):
+                    b_id = sorted_keys[i]
+                    if b_id == input_box_id:
+                        continue
+                    y = ((screen_height - 1) - i) + self.song_position
+                    x = 0
+                    self.box_positions[b_id] = (x,y)
+                self.refresh()
+                
 
     def _wait_for_input(self, expected, controller):
         """Waits for user to press correct key combination"""
@@ -167,14 +173,13 @@ class Player(object):
         # If the key was expected NOT to be pressed last state but was, the user needs to release and press again
         while expected_unset:
             expected_unset &= controller.get_pressed()
-
         input_given = 0
         last_pressed = set()
-        self.draw_input_line(last_pressed, expected)
         while input_given == 0:
             pressed = controller.get_pressed()
             if pressed.symmetric_difference(last_pressed):
-                self.draw_input_line(pressed, expected)
+                #self.draw_input_line(pressed, expected)
+                pass
             if not (expected_set - pressed):
                 input_given = self.NEXT_STATE
                 self.previously_expected_set = actual_set
@@ -189,14 +194,17 @@ class Player(object):
             elif self.flag_isset(self.RAISE_QUIT):
                 self.flags[self.RAISE_QUIT] = 0
                 input_given = self.RAISE_QUIT
-            elif self.flag_isset(self.RAISE_CHANGE):
-                self.flags[self.RAISE_CHANGE] = 0
+            elif self.flag_isset(self.RAISE_MIDI_INPUT_CHANGE):
+                self.flags[self.RAISE_MIDI_INPUT_CHANGE] = 0
                 return self._wait_for_input(expected, controller)
+            elif self.flag_isset(self.RAISE_JUMP):
+                self.flags[self.RAISE_JUMP] = 0
+                input_given = self.RAISE_JUMP
             else:
                 continue
             last_pressed = pressed
-            time.sleep(.02)
         return input_given
+
 
     def flag_isset(self, flag):
         return flag in self.flags.keys() and self.flags[flag]
@@ -204,12 +212,4 @@ class Player(object):
     def set_flag(self, flag, state=1):
         self.flags[flag] = state
     
-    def quit(self):
-        self.set_flag(self.RAISE_QUIT)
-
-    def next_state(self):
-        self.set_flag(self.NEXT_STATE)
-
-    def prev_state(self):
-        self.set_flag(self.PREV_STATE)
 
