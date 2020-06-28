9a78252c2d0127c44b422f29643e53cb68fa6705
Quintin Smith <smith.quintin@protonmail.com>
2020-06-27 17:04:18 -0700

    Fixed needing to press piano key to close program after quitting

--- MIDIController.py
+++ MIDIController.py
@@ -3,7 +3,10 @@ import os
 import time
 import mido
 
-class MIDIController(object):
+class PipeClosed(Exception):
+    pass
+
+class MIDIController:
     '''Read Input from Midi Device'''
     def __init__(self, midipath="/dev/midi1"):
         self.midipath = midipath
@@ -16,25 +19,40 @@ class MIDIController(object):
         self.flag_close = False
 
     def close(self):
-        if self.connected:
-            self.pipe.close()
+        self.flag_close = True
 
+
+    def check_byte(self):
+        output = None
+        while not output:
+            output = os.read(self.pipe.fileno(), 1)
+            if len(output) > 0:
+                output = output[0]
+            elif self.flag_close:
+                if self.connected:
+                    self.pipe.close()
+                raise PipeClosed()
+            else: #wait for input
+                time.sleep(.01)
+
+        return output
 
     def read(self):
         output = None
-        while not output and not self.flag_close:
-            byte = self.pipe.read(1)[0]
+        try:
+            byte = self.check_byte()
             if byte & 0xF0 == 0x90:
-                note = self.pipe.read(1)[0]
-                velocity = self.pipe.read(1)[0]
+                note = self.check_byte()
+                velocity = self.check_byte()
                 if velocity == 0:
                     output = mido.Message('note_off', note=note, velocity=0)
                 else:
                     output = mido.Message('note_on', note=note, velocity=velocity)
             elif byte & 0xF0 == 0x80:
-                note = self.pipe.read(1)[0]
-                velocity = self.pipe.read(1)[0]
+                note = self.check_byte()
+                velocity = self.check_byte()
                 output = mido.Message('note_off', note=note, velocity=velocity)
+        except PipeClosed:
+            output = mido.Message('stop')
 
         return output
-
