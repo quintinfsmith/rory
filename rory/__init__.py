fd012db9d817d1a0c120d36c826147a9c3d74f2e
Quintin Smith <smith.quintin@protonmail.com>
2020-11-22 16:10:59 -0800

    part of previous commit

---
+++ rory/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""
+Usage:
+    rory path/to/midi.midi
+"""
+
+__version__ = "0.2.0"
+__license__ = "GPL-2.0"
+__author__ = "Quintin Smith"
+__email__ = "smith.quintin@protonmail.com"
+__url__ = "https://github.com/quintinfsmith/rory"
