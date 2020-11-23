c361379213ddcf772be7206c21999953b0d65256
Quintin Smith <smith.quintin@protonmail.com>
2020-11-22 16:39:12 -0800

    merged __main__.py into __init__.py

--- rory/__init__.py
+++ rory/__init__.py
@@ -5,8 +5,34 @@ Usage:
     rory path/to/midi.midi
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.3"
 __license__ = "GPL-2.0"
 __author__ = "Quintin Smith"
 __email__ = "smith.quintin@protonmail.com"
 __url__ = "https://github.com/quintinfsmith/rory"
+
+def main():
+    import sys
+    import time
+    from rory.interface import Top, TerminalTooNarrow
+
+    if len(sys.argv) < 2:
+        print("Specify Midi To Play")
+        sys.exit()
+
+    try:
+        interface = Top()
+    except TerminalTooNarrow:
+        print("Terminal needs to be at least 90 characters wide")
+        sys.exit()
+
+    interface.play()
+    interface.play_along(sys.argv[1])
+
+    while interface.playing:
+        time.sleep(.4)
+
+    interface.kill()
+
+if __name__ == "__main__":
+    main()
