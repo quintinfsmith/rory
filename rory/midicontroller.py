9e100c5de35f82d03a7ae7f6018888bad9a074d6
Quintin Smith <smith.quintin@protonmail.com>
2021-01-22 08:37:19 -0800

    added animated svg for readme

--- rory/midicontroller.py
+++ rory/midicontroller.py
@@ -6,19 +6,22 @@ import pyinotify
 from apres import NoteOn, NoteOff, MIDIStop
 
 class PipeClosed(Exception):
-    pass
+    '''Error Thrown when the midi device pipe is closed or disconnected'''
 
 class TaskHandler(pyinotify.ProcessEvent):
+    '''Event hooks to connect/disconnect from newly made midi device'''
     def __init__(self, controller):
         self.controller = controller
         super().__init__()
 
     def process_IN_CREATE(self, event):
+        '''Hook to connect when midi device is plugged in'''
         if event.name[0:4] == 'midi':
             time.sleep(.5)
             self.controller.connect(event.pathname)
 
     def process_IN_DELETE(self, event):
+        '''Hook to disconnect when midi device is unplugged'''
         if event.name[0:4] == 'midi':
             self.controller.disconnect(event.pathname)
 
@@ -42,30 +45,38 @@ class MIDIController:
         notifier = pyinotify.ThreadedNotifier(self.watch_manager, TaskHandler(self))
         notifier.daemon = True
         notifier.start()
-        self.watch_manager.add_watch(MIDIController.DEVROOT, pyinotify.IN_CREATE | pyinotify.IN_DELETE)
+        self.watch_manager.add_watch(
+            MIDIController.DEVROOT,
+            pyinotify.IN_CREATE | pyinotify.IN_DELETE
+        )
 
     def is_connected(self):
+        '''Check if pipe is open and ready to be read'''
         return bool(self.pipe)
 
     def connect(self, path):
+        '''Attempt to open a pipe to the path specified'''
         if not self.pipe and os.path.exists(path):
             self.pipe = open(path, 'rb')
             self.midipath = path
 
     def disconnect(self, midipath):
+        '''Close the pipe to the path specified'''
         if self.midipath == midipath:
             try:
                 self.pipe.close()
-            except:
-                pass
+            except Exception as e:
+                raise e
 
             self.pipe = None
             self.midipath = ''
 
     def close(self):
+        '''Tear down this midi controller'''
         self.disconnect(self.midipath)
 
     def check_byte(self):
+        '''Attempt to read next byte from pipe'''
         output = None
         while not output:
             try:
@@ -75,24 +86,27 @@ class MIDIController:
             except ValueError:
                 ready = []
 
-            if not self.is_connected():
-                raise PipeClosed()
-            elif self.pipe in ready:
-                try:
-                    output = os.read(self.pipe.fileno(), 1)
-                    if len(output):
-                        output = output[0]
-                    else:
+            if self.is_connected():
+                if self.pipe in ready:
+                    try:
+                        output = os.read(self.pipe.fileno(), 1)
+                        if output:
+                            output = output[0]
+                        else:
+                            continue
+                    except ValueError:
                         continue
-                except ValueError:
-                    continue
 
-            else: #wait for input
-                time.sleep(.01)
+                else: #wait for input
+                    time.sleep(.01)
+            else:
+                raise PipeClosed()
+
 
         return output
 
     def read(self):
+        '''Read Midi Input Device until relevant event is found'''
         output = None
         try:
             byte = self.check_byte()
