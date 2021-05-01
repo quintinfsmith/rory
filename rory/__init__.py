e6ee76c1ae3a7f4851615d56428e04d140947f8e
Quintin Smith <smith.quintin@protonmail.com>
2021-05-01 14:09:21 -0700

    Version bump

--- rory/__init__.py
+++ rory/__init__.py
@@ -5,7 +5,7 @@ Usage:
     rory path/to/midi.midi
 """
 
-__version__ = "0.2.20"
+__version__ = "0.2.21"
 __license__ = "GPL-2.0"
 __author__ = "Quintin Smith"
 __email__ = "smith.quintin@protonmail.com"
